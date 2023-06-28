b7b6bee001724fd2c58f3ee1864cee3ac8df0872
yunqiyang0215 <yunqiyang@uchicago.edu>
2023-06-28 14:03:39 -0500

    modify output format

--- code/surv_susie_helper.R
+++ code/surv_susie_helper.R
@@ -31,7 +31,7 @@ surv_uni_fun <- function(x, y, o, prior_variance, estimate_intercept = 0, ...){
   sd <- summary(fit)$coefficients[1, 3]
   zscore <- bhat/sd
   lbf <- compute_lbf(zscore, sd, prior_variance)
-  lbf.corr <- lbf - bhat^2/sd^2/2+ summary(fit)$logtest[1]/2
+  lbf.corr <- lbf - bhat^2/sd^2/2+ as.numeric(summary(fit)$logtest[1]/2)
   var <- compute_approx_post_var(zscore, sd, prior_variance)
   mu <- compute_approx_post_mean(var, sd, bhat)
   return(list(mu = mu, var=var, lbf=lbf.corr, intercept=0))
