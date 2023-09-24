1b5f04a18c03740be11bf903bed01ed6a96cbfad
yunqiyang0215 <yunqiyang@uchicago.edu>
2023-09-24 11:59:00 -0500

    add sd to survuni fun

--- code/surv_susie_helper.R
+++ code/surv_susie_helper.R
@@ -34,6 +34,6 @@ surv_uni_fun <- function(x, y, o, prior_variance, estimate_intercept = 0, ...){
   lbf.corr <- lbf - bhat^2/sd^2/2+ as.numeric(summary(fit)$logtest[1]/2)
   var <- compute_approx_post_var(zscore, sd, prior_variance)
   mu <- compute_approx_post_mean(var, sd, bhat)
-  return(list(mu = mu, var=var, lbf=lbf.corr, intercept=0))
+  return(list(mu = mu, var=var, sd = sd, lbf=lbf.corr, intercept=0))
 }
 
